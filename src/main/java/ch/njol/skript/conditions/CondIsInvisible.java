02d2a95232f1b45fbe178618261b54da5527a335
Moderocky <admin@moderocky.com>
2024-08-20 13:13:06 +0100

    Add visibility syntax stuff.

--- src/main/java/ch/njol/skript/conditions/CondIsInvisible.java
+++ src/main/java/ch/njol/skript/conditions/CondIsInvisible.java
@@ -27,35 +27,41 @@ import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
+import org.bukkit.boss.BossBar;
 import org.bukkit.entity.LivingEntity;
 
 @Name("Is Invisible")
-@Description("Checks whether a living entity is invisible.")
+@Description("Checks whether a living entity or a boss bar is invisible.")
 @Examples("target entity is invisible")
 @Since("2.7")
-public class CondIsInvisible extends PropertyCondition<LivingEntity> {
+public class CondIsInvisible extends PropertyCondition<Object> {
 
 	static {
 		if (Skript.methodExists(LivingEntity.class, "isInvisible"))
-			register(CondIsInvisible.class, PropertyType.BE, "(invisible|:visible)", "livingentities");
+			register(CondIsInvisible.class, PropertyType.BE, "(invisible|:visible)", "livingentities/bossbars");
 	}
 
+	private boolean visible;
+
 	@Override
-	@SuppressWarnings("unchecked")
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-		setExpr((Expression<LivingEntity>) exprs[0]);
-		setNegated(matchedPattern == 1 ^ parseResult.hasTag("visible"));
+		setExpr(exprs[0]);
+		setNegated(matchedPattern == 1 ^ (visible = parseResult.hasTag("visible")));
 		return true;
 	}
 
 	@Override
-	public boolean check(LivingEntity livingEntity) {
-		return livingEntity.isInvisible();
+	public boolean check(Object target) {
+		if (target instanceof LivingEntity entity)
+			return entity.isInvisible();
+		else if (target instanceof BossBar bar)
+			return !bar.isVisible();
+		return false;
 	}
 
 	@Override
 	protected String getPropertyName() {
-		return "invisible";
+		return visible ? "visible" : "invisible";
 	}
 
 }
--- src/main/java/ch/njol/skript/effects/EffInvisible.java
+++ src/main/java/ch/njol/skript/effects/EffInvisible.java
@@ -27,13 +27,14 @@ import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
+import org.bukkit.boss.BossBar;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.Event;
 import org.eclipse.jdt.annotation.Nullable;
 
 @Name("Make Invisible")
 @Description({
-	"Makes a living entity visible/invisible. This is not a potion and therefore does not have features such as a time limit or particles.",
+	"Makes a living entity or a boss bar visible/invisible. This is not a potion and therefore does not have features such as a time limit or particles.",
 	"When setting an entity to invisible while using an invisibility potion on it, the potion will be overridden and when it runs out the entity keeps its invisibility."
 })
 @Examples("make target entity invisible")
@@ -43,30 +44,34 @@ public class EffInvisible extends Effect {
 	static {
 		if (Skript.methodExists(LivingEntity.class, "isInvisible"))
 			Skript.registerEffect(EffInvisible.class,
-				"make %livingentities% (invisible|not visible)",
-				"make %livingentities% (visible|not invisible)");
+				"make %livingentities/bossbars% (invisible|not visible)",
+				"make %livingentities/bossbars% (visible|not invisible)");
 	}
 
 	private boolean invisible;
-	private Expression<LivingEntity> livingEntities;
+	private Expression<Object> targets;
 
 	@Override
 	@SuppressWarnings("unchecked")
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-		livingEntities = (Expression<LivingEntity>) exprs[0];
+		targets = (Expression<Object>) exprs[0];
 		invisible = matchedPattern == 0;
 		return true;
 	}
 
 	@Override
 	protected void execute(Event event) {
-		for (LivingEntity entity : livingEntities.getArray(event))
-			entity.setInvisible(invisible);
+		for (Object target : targets.getArray(event)) {
+			if (target instanceof LivingEntity entity)
+				entity.setInvisible(invisible);
+			else if (target instanceof BossBar bar)
+				bar.setVisible(!invisible);
+		}
 	}
 
 	@Override
 	public String toString(@Nullable Event event, boolean debug) {
-		return "make " + livingEntities.toString(event, debug) + " " + (invisible ? "in" : "") + "visible";
+		return "make " + targets.toString(event, debug) + " " + (invisible ? "in" : "") + "visible";
 	}
 
 }
