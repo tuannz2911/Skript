3a137b629ac3414fbd5e65ae82c1492933ba5505
Moderocky <admin@moderocky.com>
2024-09-04 18:08:54 +0100

    Apply suggestions from code review
    Co-authored-by: sovdee <10354869+sovdeeth@users.noreply.github.com>

--- src/main/java/ch/njol/skript/effects/EffAnnotate.java
+++ src/main/java/ch/njol/skript/effects/EffAnnotate.java
@@ -1,21 +1,3 @@
-/**
- *   This file is part of Skript.
- *
- *  Skript is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Skript is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright Peter Güttinger, SkriptLang team and contributors
- */
 package ch.njol.skript.effects;
 
 import ch.njol.skript.Skript;
@@ -45,9 +27,9 @@ import org.skriptlang.skript.lang.script.Annotation;
 })
 @Examples({
 	"on join:",
-	"\t@suppress warnings",
-	"\t@suppress errors",
-	"\tbroadcast \"hello there!\""
+		"\t@suppress warnings",
+		"\t@suppress errors",
+		"\tbroadcast \"hello there!\""
 })
 @Since("INSERT VERSION")
 public class EffAnnotate extends Effect implements MetaSyntaxElement {
