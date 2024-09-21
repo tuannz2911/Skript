3e3f0a9d77e4e2655162ee2cb8beff11b1f31cdf
sovdee <10354869+sovdeeth@users.noreply.github.com>
2024-09-21 17:55:48 -0400

    bstats - Fix trying to compare enum value to string value for verbosity and event priority (#7027)
    * Fix trying to compare enum value to string value for verbosity and event priority
    
    * wrong polarity for var/command case sensitivity

--- src/main/java/org/skriptlang/skript/bukkit/SkriptMetrics.java
+++ src/main/java/org/skriptlang/skript/bukkit/SkriptMetrics.java
@@ -120,12 +120,14 @@ public class SkriptMetrics {
 
 		metrics.addCustomChart(new DrilldownPie("drilldownLogVerbosity", () -> {
 			String verbosity = SkriptConfig.verbosity.value().name().toLowerCase(Locale.ENGLISH).replace('_', ' ');
-			return isDefaultMap(verbosity, SkriptConfig.verbosity.defaultValue());
+			String defaultValue = SkriptConfig.verbosity.defaultValue().name().toLowerCase(Locale.ENGLISH).replace('_', ' ');
+			return isDefaultMap(verbosity, defaultValue);
 		}));
 
 		metrics.addCustomChart(new DrilldownPie("drilldownPluginPriority", () -> {
 			String priority = SkriptConfig.defaultEventPriority.value().name().toLowerCase(Locale.ENGLISH).replace('_', ' ');
-			return isDefaultMap(priority, SkriptConfig.defaultEventPriority.defaultValue());
+			String defaultValue = SkriptConfig.defaultEventPriority.defaultValue().name().toLowerCase(Locale.ENGLISH).replace('_', ' ');
+			return isDefaultMap(priority, defaultValue);
 		}));
 		metrics.addCustomChart(new SimplePie("cancelledByDefault", () ->
 			SkriptConfig.listenCancelledByDefault.value().toString()
@@ -143,10 +145,10 @@ public class SkriptMetrics {
 			SkriptConfig.caseSensitive.value().toString()
 		));
 		metrics.addCustomChart(new SimplePie("caseSensitiveVariables", () ->
-			SkriptConfig.caseInsensitiveVariables.value().toString()
+			String.valueOf(!SkriptConfig.caseInsensitiveVariables.value())
 		));
 		metrics.addCustomChart(new SimplePie("caseSensitiveCommands", () ->
-			SkriptConfig.caseInsensitiveCommands.value().toString()
+			String.valueOf(!SkriptConfig.caseInsensitiveCommands.value())
 		));
 
 		metrics.addCustomChart(new SimplePie("disableSaveWarnings", () ->
