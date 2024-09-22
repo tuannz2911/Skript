98013299019f9f7a32e3ab615e4a3a62ece4b95c
TenFont <83959297+TenFont@users.noreply.github.com>
2024-09-22 05:32:43 +0500

    Return isNegative() and not false, if CondIsWithin is invalid (#7038)
    * Return isNegated() instead of false if check is invalid
    
    * Add test for <1.17 as well
    
    ---------
    
    Co-authored-by: sovdee <10354869+sovdeeth@users.noreply.github.com>

--- src/main/java/ch/njol/skript/conditions/CondIsWithin.java
+++ src/main/java/ch/njol/skript/conditions/CondIsWithin.java
@@ -103,7 +103,7 @@ public class CondIsWithin extends Condition {
 			Location one = loc1.getSingle(event);
 			Location two = loc2.getSingle(event);
 			if (one == null || two == null || one.getWorld() != two.getWorld())
-				return false;
+				return isNegated();
 			AABB box = new AABB(one, two);
 			return locsToCheck.check(event, box::contains, isNegated());
 		}
@@ -111,7 +111,7 @@ public class CondIsWithin extends Condition {
 		// else, within an entity/block/chunk/world
 		Object area = this.area.getSingle(event);
 		if (area == null)
-			return false;
+			return isNegated();
 
 		// Entities
 		if (area instanceof Entity) {
